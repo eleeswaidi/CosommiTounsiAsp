24df8d2c6ed69ad8d639d1519c674b50b9d28caf
eleeswaidi <elee.swaidi@esprit.tn>
2021-04-21 23:10:30 +0100

    test

---
+++ ConsommiTounsii/Controllers/DeliveryController.cs
@@ -0,0 +1,105 @@
+﻿using ConsommiTounsii.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsommiTounsii.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        HttpClient Client;
+        public DeliveryController()
+        {
+             Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:8083/ConsomiTounsi/servlet");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+        }
+
+        // GET: Delivery
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        // GET: Delivery/Details/5
+        public ActionResult Details(int id)
+        {
+
+            return View();
+        }
+
+        // GET: Delivery/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Delivery/Create
+        [HttpPost]
+        public ActionResult Create(Delivery delivery)
+        {
+            try
+            {
+                HttpResponseMessage response = Client.GetAsync("api/employes").Result;
+                if (response)
+                    // TODO: Add insert logic here
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Delivery/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Delivery/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Delivery/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Delivery/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
